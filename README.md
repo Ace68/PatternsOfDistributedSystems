a7cbd5f3aa4e5244b6e0c6f76968a1604fa767ac
Alberto Acerbis <alberto.acerbis@gmail.com>
2024-03-01 17:05:10 +0100

    Add Metrics to Warehouses Add Locust for stress test

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # GlobalAzure-2024
 Patterns of Distributed Systems (Fitness Functions, Chaos, Resilience, Metrics)
+
+## Metrics
+
+## Locust
+Locust is an open source load testing tool.  
+Here you can find the documentation: <a href="https://locust.io//" target="_blank">Locust</a>  
+To run test, under locst folder, run  
+locust -f LocustBrewUp.py  
+Then navigate to http://localhost:8089
---
+++ locoust/LocustBrewUp.py
@@ -0,0 +1,50 @@
+from locust import task, run_single_user
+from locust import FastHttpUser
+
+
+class Sales(FastHttpUser):
+    host = "http://localhost:5500"
+    default_headers = {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Connection": "keep-alive",
+        "Cookie": "grafana_session=67a0d7596a5adb9c65234ab3b2982d68; grafana_session_expiry=1709305914",
+        "Host": "localhost:5500",
+        "Referer": "http://localhost:5500/documentation/index.html",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0",
+    }
+
+    @task
+    def t(self):
+        with self.rest("GET", "/v1/sales") as resp:
+            pass
+
+class Warehouses(FastHttpUser):
+    host = "http://localhost:5600"
+    default_headers = {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Connection": "keep-alive",
+        "Cookie": "grafana_session=67a0d7596a5adb9c65234ab3b2982d68; grafana_session_expiry=1709305914",
+        "Host": "localhost:5500",
+        "Referer": "http://localhost:5500/documentation/index.html",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0",
+    }
+
+    @task
+    def t(self):
+        with self.rest("GET", "/v1/wareHouses/availabilities/") as resp:
+            pass
+
+
+if __name__ == "__main__":
+    run_single_user(Sales)
+    run_single_user(Warehouses)
